4ddd5b9ce504e3e7ec11a8c09a977e886f35dbfd
samghadri <sam.ghadri@gmail.com>
2017-12-28 23:46:56 +0000

    modelsTagsStartupsPostNewLinks

---
+++ suorganizer/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.text import slugify
+from organizer.models import Tag, StartUp
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50,
+                            unique_for_month='pub_date',
+                            help_text = 'A label for URL config')
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(StartUp, related_name='blog_posts')
+
+
+    def __str__(self):
+        return '{} on {}'.format(
+                    self.title,
+                    self.pub_date.strftime('%y-%m-%d'))
---
+++ suorganizer/organizer/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+# from blog.models import Post
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(max_length=40,
+                            unique=True,
+                            help_text='A label for URL config')
+
+    def __str__(self):
+        return self.name
+
+
+
+class StartUp(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(unique=True,
+                            help_text='A label for URL config')
+    description = models.TextField()
+    founded_date = models.DateField('date founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=70)
+    pub_date = models.DateField('date published')
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(StartUp)
+
+    def __str__(self):
+        return '{}:{}'.format(self.startup, self.title)
