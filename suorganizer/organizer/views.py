0dc1def5540af6088aa7a6d37e58d50b161f3e29
samghadri <sam.ghadri@gmail.com>
2017-12-31 16:19:47 +0000

    startingViews

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from .models import Tag
+from django.template import loader, Context
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = '  --  '.join(tag.name for tag in tag_list)
+    template = loader.get_template('organizer/tag_list.html')
+    context = {'tag_list': tag_list}
+    output = template.render(context)
     return HttpResponse(output)
