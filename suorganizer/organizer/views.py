5b89b19b7109ba84628d70a943e23aced5c59ba1
samghadri <sam.ghadri@gmail.com>
2018-01-29 22:15:52 +0000

    CompletePaginationStartup

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -73,9 +73,29 @@ class StartupList(View):
     def get(self, request):
         startups = StartUp.objects.all()
         paginator = Paginator(startups, self.paginate_by)
-        page = paginator.page(1)
+        page_number = request.GET.get(self.page_kwarg)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(paginator.num_pages)
+
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(pkw=self.page_kwarg,
+                        n=page.previous_page_number())
+        else:
+            prev_url = None
+
+        if page.has_next():
+            next_url = '?{pkw}={n}'.format(pkw=self.page_kwarg,
+                        n=page.next_page_number())
+        else:
+            next_url = None
         context = {'is_paginated': page.has_other_pages(),
+                   'next_page_url': next_url,
                    'paginator': paginator,
+                   'previous_page_url': prev_url,
                    'startup_list': page}
         return render(request, self.template_name, context)
 
